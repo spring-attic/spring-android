c172eae927e684968ea7350434e7771c56f49aa7
Roy Clarkson <rclarkson@vmware.com>
2010-10-26 01:23:21 -0400

    Added empty project

---
+++ src/main/java/org/springframework/android/App.java
@@ -0,0 +1,13 @@
+package org.springframework.android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
