c955f4f74d98e1b11e7682276ea517bd8d9575a2
Roy Clarkson <rclarkson@vmware.com>
2010-10-28 14:13:34 -0400

    Added initial framework for a commons-logging implementation. The AndroidLogFactory class is in progress.

---
+++ commons-logging/src/main/java/org/apache/commons/logging/impl/AndroidLogFactory.java
@@ -0,0 +1,55 @@
+package org.apache.commons.logging.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogConfigurationException;
+import org.apache.commons.logging.LogFactory;
+
+public class AndroidLogFactory extends LogFactory {
+	
+	public AndroidLogFactory() {
+		
+	}
+
+	@Override
+	public Object getAttribute(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getAttributeNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Log getInstance(Class clazz) throws LogConfigurationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Log getInstance(String name) throws LogConfigurationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void release() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAttribute(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAttribute(String name, Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
