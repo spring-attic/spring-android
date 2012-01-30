97f1a3b53002b586c3e7605be342c78813cb89fd
Roy Clarkson <rclarkson@vmware.com>
2012-01-30 13:21:48 -0500

    Updated SourceHttpMessageConverterTests to log warnings instead of failing on versions older than Froyo (Android 2.2).

--- spring-android-rest-template-test/src/org/springframework/http/converter/xml/SourceHttpMessageConverterTests.java
+++ spring-android-rest-template-test/src/org/springframework/http/converter/xml/SourceHttpMessageConverterTests.java
@@ -31,6 +31,7 @@ import junit.framework.TestCase;
 import org.springframework.http.MediaType;
 import org.springframework.http.MockHttpInputMessage;
 import org.springframework.http.MockHttpOutputMessage;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.FileCopyUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -39,126 +40,151 @@ import org.xml.sax.InputSource;
 
 import android.os.Build;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.util.Log;
 
-/** 
+/**
  * @author Arjen Poutsma
- * @author Roy Clarkson 
+ * @author Roy Clarkson
  */
 public class SourceHttpMessageConverterTests extends TestCase {
-	
-	private static final boolean javaxXmlTransformPresent = 
-		(Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO);
 
-	private SourceHttpMessageConverter<Source> converter;
+    private static final String TAG = SourceHttpMessageConverterTests.class.getSimpleName();
 
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		if (javaxXmlTransformPresent) {
-			converter = new SourceHttpMessageConverter<Source>();
-		} else {
-			fail("SourceHttpMessageConverter is not compatible with this version of Android");
-		}
-	}
-	
-	@Override
-	public void tearDown() {
-		converter = null;
-	}
+    private static final boolean javaxXmlTransformPresent = 
+            ClassUtils.isPresent("javax.xml.transform.Source", SourceHttpMessageConverterTests.class.getClassLoader());
 
-	@SmallTest
-	public void testCanRead() {
-		assertTrue(converter.canRead(Source.class, new MediaType("application", "xml")));
-		assertTrue(converter.canRead(Source.class, new MediaType("application", "soap+xml")));
-	}
+    private static final boolean olderThanFroyo = 
+            (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO);
 
-	@SmallTest
-	public void testCanWrite() {
-		assertTrue(converter.canWrite(Source.class, new MediaType("application", "xml")));
-		assertTrue(converter.canWrite(Source.class, new MediaType("application", "soap+xml")));
-		assertTrue(converter.canWrite(Source.class, MediaType.ALL));
-	}
+    private SourceHttpMessageConverter<Source> converter;
 
-	@SmallTest
-	public void testReadDOMSource() throws Exception {
-		String body = "<root>Hello World</root>";
-		MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
-		inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
-		DOMSource result = (DOMSource) converter.read(DOMSource.class, inputMessage);
-		Document document = (Document) result.getNode();
-		assertEquals("Invalid result", "root", document.getDocumentElement().getLocalName());
-	}
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        if (javaxXmlTransformPresent) {
+            this.converter = new SourceHttpMessageConverter<Source>();
+        } else {
+            assertTrue(olderThanFroyo);
+            Log.w(TAG, "SourceHttpMessageConverter is not compatible with this version of Android");
+        }
+    }
 
-	@SmallTest
-	public void testReadSAXSource() throws Exception {
-		String body = "<root>Hello World</root>";
-		MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
-		inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
-		SAXSource result = (SAXSource) converter.read(SAXSource.class, inputMessage);
-		InputSource inputSource = result.getInputSource();
-		String s = FileCopyUtils.copyToString(new InputStreamReader(inputSource.getByteStream()));
-		assertTrue("Invalid result", s.contains(body));
-	}
+    @Override
+    public void tearDown() {
+        this.converter = null;
+    }
 
-	@SmallTest
-	public void testReadStreamSource() throws Exception {
-		String body = "<root>Hello World</root>";
-		MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
-		inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
-		StreamSource result = (StreamSource) converter.read(StreamSource.class, inputMessage);
-		String s = FileCopyUtils.copyToString(new InputStreamReader(result.getInputStream()));
-		assertTrue("Invalid result", s.contains(body));
-	}
+    @SmallTest
+    public void testCanRead() {
+        if (javaxXmlTransformPresent) {
+            assertTrue(converter.canRead(Source.class, new MediaType("application", "xml")));
+            assertTrue(converter.canRead(Source.class, new MediaType("application", "soap+xml")));
+        }
+    }
 
-	@SmallTest
-	public void testReadSource() throws Exception {
-		String body = "<root>Hello World</root>";
-		MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
-		inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
-		converter.read(Source.class, inputMessage);
-	}
+    @SmallTest
+    public void testCanWrite() {
+        if (javaxXmlTransformPresent) {
+            assertTrue(converter.canWrite(Source.class, new MediaType("application", "xml")));
+            assertTrue(converter.canWrite(Source.class, new MediaType("application", "soap+xml")));
+            assertTrue(converter.canWrite(Source.class, MediaType.ALL));
+        }
+    }
 
-	@SmallTest
-	public void testWriteDOMSource() throws Exception {
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		documentBuilderFactory.setNamespaceAware(true);
-		Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
-		Element rootElement = document.createElement("root");
-		document.appendChild(rootElement);
-		Text text = document.createTextNode("Hello World");
-		rootElement.appendChild(text);
-		DOMSource domSource = new DOMSource(document);
+    @SmallTest
+    public void testReadDOMSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String body = "<root>Hello World</root>";
+            MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
+            inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
+            DOMSource result = (DOMSource) converter.read(DOMSource.class, inputMessage);
+            Document document = (Document) result.getNode();
+            assertEquals("Invalid result", "root", document.getDocumentElement().getLocalName());
+        }
+    }
 
-		MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
-		converter.write(domSource, null, outputMessage);
-		String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
-		assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
-		assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
-		assertEquals("Invalid content-length", outputMessage.getBodyAsBytes().length, outputMessage.getHeaders().getContentLength());
-	}
-	
-	@SmallTest
-	public void testWriteSAXSource() throws Exception {
-		String xml = "<root>Hello World</root>";
-		SAXSource saxSource = new SAXSource(new InputSource(new StringReader(xml)));
+    @SmallTest
+    public void testReadSAXSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String body = "<root>Hello World</root>";
+            MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
+            inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
+            SAXSource result = (SAXSource) converter.read(SAXSource.class, inputMessage);
+            InputSource inputSource = result.getInputSource();
+            String s = FileCopyUtils.copyToString(new InputStreamReader(inputSource.getByteStream()));
+            assertTrue("Invalid result", s.contains(body));
+        }
+    }
 
-		MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
-		converter.write(saxSource, null, outputMessage);
-		String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
-		assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
-		assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
-	}
+    @SmallTest
+    public void testReadStreamSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String body = "<root>Hello World</root>";
+            MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
+            inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
+            StreamSource result = (StreamSource) converter.read(StreamSource.class, inputMessage);
+            String s = FileCopyUtils.copyToString(new InputStreamReader(result.getInputStream()));
+            assertTrue("Invalid result", s.contains(body));
+        }
+    }
 
-	@SmallTest
-	public void testWriteStreamSource() throws Exception {
-		String xml = "<root>Hello World</root>";
-		StreamSource streamSource = new StreamSource(new StringReader(xml));
+    @SmallTest
+    public void testReadSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String body = "<root>Hello World</root>";
+            MockHttpInputMessage inputMessage = new MockHttpInputMessage(body.getBytes("UTF-8"));
+            inputMessage.getHeaders().setContentType(new MediaType("application", "xml"));
+            converter.read(Source.class, inputMessage);
+        }
+    }
 
-		MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
-		converter.write(streamSource, null, outputMessage);
-		String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
-		assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
-		assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
-	}
+    @SmallTest
+    public void testWriteDOMSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setNamespaceAware(true);
+            Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
+            Element rootElement = document.createElement("root");
+            document.appendChild(rootElement);
+            Text text = document.createTextNode("Hello World");
+            rootElement.appendChild(text);
+            DOMSource domSource = new DOMSource(document);
+
+            MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
+            converter.write(domSource, null, outputMessage);
+            String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
+            assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
+            assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
+            assertEquals("Invalid content-length", outputMessage.getBodyAsBytes().length, outputMessage.getHeaders().getContentLength());
+        }
+    }
+
+    @SmallTest
+    public void testWriteSAXSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String xml = "<root>Hello World</root>";
+            SAXSource saxSource = new SAXSource(new InputSource(new StringReader(xml)));
+
+            MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
+            converter.write(saxSource, null, outputMessage);
+            String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
+            assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
+            assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
+        }
+    }
+
+    @SmallTest
+    public void testWriteStreamSource() throws Exception {
+        if (javaxXmlTransformPresent) {
+            String xml = "<root>Hello World</root>";
+            StreamSource streamSource = new StreamSource(new StringReader(xml));
+
+            MockHttpOutputMessage outputMessage = new MockHttpOutputMessage();
+            converter.write(streamSource, null, outputMessage);
+            String s = outputMessage.getBodyAsString(Charset.forName("UTF-8"));
+            assertTrue("Invalid result", s.contains("<root>Hello World</root>"));
+            assertEquals("Invalid content-type", new MediaType("application", "xml"), outputMessage.getHeaders().getContentType());
+        }
+    }
 
 }
