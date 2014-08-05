ae82828db723ca5bb55f0cd7625ad4c5946722b3
Roy Clarkson <rclarkson@pivotal.io>
2014-08-05 16:16:26 +0300

    Format code and whitespace

--- test/spring-android-rest-template-test/src/main/java/org/springframework/android/test/Assert.java
+++ test/spring-android-rest-template-test/src/main/java/org/springframework/android/test/Assert.java
@@ -22,49 +22,51 @@ import junit.framework.TestCase;
  * @author Roy Clarkson
  */
 public class Assert extends TestCase {
-	
+
 	private static final String MESSAGE = "arrays are not equal";
-	
-	public static void assertArrayEquals(Object[] expecteds, Object[] actuals)  {
-		assertArrayEquals(MESSAGE, expecteds, actuals);
+
+	public static void assertArrayEquals(Object[] expected, Object[] actual) {
+		assertArrayEquals(MESSAGE, expected, actual);
 	}
-	
-	public static void assertArrayEquals(String message, Object[] expecteds, Object[] actuals)  {
+
+	public static void assertArrayEquals(String message, Object[] expected,
+			Object[] actual) {
+		assertNotNull(expected);
+		assertNotNull(actual);
+		assertEquals(expected.length, actual.length);
+		if (expected.length == actual.length) {
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], actual[i]);
+			}
+		}
+	}
+
+	public static void assertArrayEquals(String[] expected, String[] actual) {
+		assertArrayEquals(MESSAGE, expected, actual);
+	}
+
+	public static void assertArrayEquals(String message, String[] expecteds,
+			String[] actuals) {
 		assertNotNull(expecteds);
 		assertNotNull(actuals);
 		assertEquals(expecteds.length, actuals.length);
 		if (expecteds.length == actuals.length) {
-			for (int i = 0; i < expecteds.length; i++ ) {
+			for (int i = 0; i < expecteds.length; i++) {
 				assertEquals(expecteds[i], actuals[i]);
 			}
 		}
 	}
-	
-	public static void assertArrayEquals(String[] expecteds, String[] actuals)  {
-		assertArrayEquals(MESSAGE, expecteds, actuals);
+
+	public static void assertArrayEquals(byte[] expected, byte[] actual) {
+		assertArrayEquals(MESSAGE, expected, actual);
 	}
-	
-	public static void assertArrayEquals(String message, String[] expecteds, String[] actuals)  {
+
+	public static void assertArrayEquals(String message, byte[] expecteds, byte[] actuals) {
 		assertNotNull(expecteds);
 		assertNotNull(actuals);
 		assertEquals(expecteds.length, actuals.length);
 		if (expecteds.length == actuals.length) {
-			for (int i = 0; i < expecteds.length; i++ ) {
-				assertEquals(expecteds[i], actuals[i]);
-			}
-		}
-	}
-	
-	public static void assertArrayEquals(byte[] expecteds, byte[] actuals)  {
-		assertArrayEquals(MESSAGE, expecteds, actuals);
-	}
-	
-	public static void assertArrayEquals(String message, byte[] expecteds, byte[] actuals)  {
-		assertNotNull(expecteds);
-		assertNotNull(actuals);
-		assertEquals(expecteds.length, actuals.length);
-		if (expecteds.length == actuals.length) {
-			for (int i = 0; i < expecteds.length; i++ ) {
+			for (int i = 0; i < expecteds.length; i++) {
 				assertEquals(expecteds[i], actuals[i]);
 			}
 		}
