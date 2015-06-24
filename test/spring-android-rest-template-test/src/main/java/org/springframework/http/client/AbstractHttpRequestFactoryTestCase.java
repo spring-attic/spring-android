2f21201171af225a11635ba6904da3314ca47055
Roy Clarkson <rclarkson@pivotal.io>
2015-06-24 12:40:15 -0500

    Update tests to OkHttp 2.4.0

--- test/spring-android-rest-template-test/src/main/java/org/springframework/http/client/AbstractHttpRequestFactoryTestCase.java
+++ test/spring-android-rest-template-test/src/main/java/org/springframework/http/client/AbstractHttpRequestFactoryTestCase.java
@@ -292,6 +292,30 @@ public abstract class AbstractHttpRequestFactoryTestCase extends TestCase {
 		assertHttpMethod("delete", HttpMethod.DELETE);
 	}
 
+	protected void assertHttpMethod(String path, HttpMethod method) throws Exception {
+		ClientHttpResponse response = null;
+		try {
+			ClientHttpRequest request = factory.createRequest(new URI(baseUrl + "/methods/" + path), method);
+			if (method == HttpMethod.POST || method == HttpMethod.PUT || method == HttpMethod.PATCH) {
+				// requires a body
+				try {
+					request.getBody().write(32);
+				}
+				catch (UnsupportedOperationException ex) {
+					// probably a streaming request - let's simply ignore it
+				}
+			}
+			response = request.execute();
+			assertEquals("Invalid response status", HttpStatus.OK, response.getStatusCode());
+			assertEquals("Invalid method", path.toUpperCase(Locale.ENGLISH), request.getMethod().name());
+		}
+		finally {
+			if (response != null) {
+				response.close();
+			}
+		}
+	}
+
 	@MediumTest
 	public void testGetAcceptEncodingGzip() throws Exception {
 		ClientHttpRequest request = factory.createRequest(new URI(baseUrl + "/gzip"), HttpMethod.GET);
@@ -445,21 +469,6 @@ public abstract class AbstractHttpRequestFactoryTestCase extends TestCase {
 			assertEquals("Invalid status code", HttpStatus.OK, response.getStatusCode());
 		} finally {
 			response.close();
-		}
-	}
-
-	protected void assertHttpMethod(String path, HttpMethod method) throws Exception {
-		ClientHttpResponse response = null;
-		try {
-			ClientHttpRequest request = factory.createRequest(new URI(baseUrl + "/methods/" + path), method);
-			response = request.execute();
-			assertEquals("Invalid response status", HttpStatus.OK, response.getStatusCode());
-			assertEquals("Invalid method", path.toUpperCase(Locale.ENGLISH), request.getMethod().name());
-		}
-		finally {
-			if (response != null) {
-				response.close();
-			}
 		}
 	}
 
